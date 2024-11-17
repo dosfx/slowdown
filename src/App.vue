e804b47ef293e52aeb6cd83c0cd5e0afb9048d2e
dosfx <andrew.adbrown@gmail.com>
2024-11-16 21:37:15 -0500

    forgot settings icon

--- src/App.vue
+++ src/App.vue
@@ -139,7 +139,12 @@ const lockActive = activeColor(lockSetting);
                     d="M144 144l0 48 160 0 0-48c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192l0-48C80 64.5 144.5 0 224 0s144 64.5 144 144l0 48 16 0c35.3 0 64 28.7 64 64l0 192c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 256c0-35.3 28.7-64 64-64l16 0z" />
             </svg>
         </Button>
-        <Button @click="settingsScreen?.show"></Button>
+        <Button @click="settingsScreen?.show">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="60%">
+                <path
+                    d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
+            </svg>
+        </Button>
     </footer>
     <LockScreen ref="lockScreen" :current @exit="resetInterval" />
     <WakeLockAlert />
